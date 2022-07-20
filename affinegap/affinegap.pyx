289737cd782e381c9025e1e307d0ab171db0a0f0
Sebastien Iooss <archimist.linux@gmail.com>
2022-07-20 14:31:31 +0200

    remove parameters

--- affinegap/affinegap.pyx
+++ affinegap/affinegap.pyx
@@ -17,10 +17,7 @@ from libc.stdlib cimport free, malloc
 from libcpp.vector cimport vector
 
 
-cpdef float affineGapDistanceInputOrder(const int[::1]& int_memview_1, const int[::1]& int_memview_2, const int length1, const int length2,
-                              const float gapWeight,
-                              const float spaceWeight,
-                              const float abbreviation_scale):
+cpdef float affineGapDistanceInputOrder(const int[::1]& int_memview_1, const int[::1]& int_memview_2, const int length1, const int length2):
     # suppose len(int_memview_1) <= len(int_memview_2)
     cdef const int * int_ptr_1 = &int_memview_1[0]
     cdef const int * int_ptr_2 = &int_memview_2[0]
@@ -42,40 +39,32 @@ cpdef float affineGapDistanceInputOrder(const int[::1]& int_memview_1, const int
     # D(0,j) = Infinity
     V_current[0] = 0
     for j in range(1, length1 + 1) :
-        V_current[j] = gapWeight + spaceWeight * j
+        V_current[j] = 0.5 + 0.5 * j
         D[j] = limits.INT_MAX
 
     for i in range(1, length2 +1) :
 
-        # V_previous = V_current
+        # V_previous, V_current = V_current, V_previous
         for _ in range(0, length1 + 1) :
             V_previous[_] = V_current[_]
 
         # Base conditions
-        # V(i,0) = gapWeight + spaceWeight * i
+        # V(i,0) = 0.5 + 0.5 * i
         # I(i,0) = Infinity
-        V_current[0] = gapWeight + spaceWeight * i
+        V_current[0] = 0.5 + 0.5 * i
         I = limits.INT_MAX
 
         for j in range(1, length1+1) :
 
             # I(i,j) is the edit distance if the jth character of string 1
             # was inserted into string 2.
-            #
-            # I(i,j) = min(I(i,j-1), V(i,j-1) + gapWeight) + spaceWeight
-            if j <= length2 :
-                I = min(I, V_current[j-1] + gapWeight) + spaceWeight
-            else :
-                # Pay less for abbreviations
-                # i.e. 'spago (los angeles) to 'spago'
-                I = (min(I, V_current[j-1] + gapWeight * abbreviation_scale)
-                     + spaceWeight * abbreviation_scale)
+            I = min(I, V_current[j-1] + 0.5) + 0.5
 
             # D(i,j) is the edit distance if the ith character of string 2
             # was deleted from string 1
             #
-            # D(i,j) = min((i-1,j), V(i-1,j) + gapWeight) + spaceWeight
-            D[j] = min(D[j], V_previous[j] + gapWeight) + spaceWeight
+            # D(i,j) = min((i-1,j), V(i-1,j) + 0.5) + 0.5
+            D[j] = min(D[j], V_previous[j] + 0.5) + 0.5
 
             # M(i,j) is the edit distance if the ith and jth characters
             # match or mismatch
@@ -100,10 +89,7 @@ cpdef float affineGapDistanceInputOrder(const int[::1]& int_memview_1, const int
     return distance
 
 
-cpdef float affineGapDistance(int[::1] string_a, int[::1] string_b,
-                              const float gapWeight,
-                              const float spaceWeight,
-                              const float abbreviation_scale):
+cpdef float affineGapDistance(int[::1] string_a, int[::1] string_b):
     """
     Calculate the affine gap distance between two strings
 
@@ -119,23 +105,14 @@ cpdef float affineGapDistance(int[::1] string_a, int[::1] string_b,
     if length1 < length2 :
         return affineGapDistanceInputOrder(string_b, string_a,
                               length2,
-                              length1,
-                              gapWeight,
-                              spaceWeight,
-                              abbreviation_scale)
+                              length1)
     else:
         return affineGapDistanceInputOrder(string_a, string_b,
                               length1,
-                              length2,
-                              gapWeight,
-                              spaceWeight,
-                              abbreviation_scale)
+                              length2)
 
 
-cpdef float normalizedAffineGapDistance(int[::1] string_a, int[::1] string_b,
-                                        const float gapWeight,
-                                        const float spaceWeight,
-                                        const float abbreviation_scale) except? 999 :
+cpdef float normalizedAffineGapDistance(int[::1] string_a, int[::1] string_b) except? 999 :
 
     cdef int length1 = string_a.shape[0]
     cdef int length2 = string_b.shape[0]
@@ -145,26 +122,17 @@ cpdef float normalizedAffineGapDistance(int[::1] string_a, int[::1] string_b,
     if normalizer == 0:
         raise ZeroDivisionError('normalizedAffineGapDistance cannot take two empty strings')
 
-    cdef float distance = affineGapDistance(string_a, string_b,
-                                            gapWeight,
-                                            spaceWeight,
-                                            abbreviation_scale)
+    cdef float distance = affineGapDistance(string_a, string_b)
 
     return distance/normalizer
 
-cpdef vector[float] affinaGapDistanceArray(vector[int[::1]] strings,
-                              const float gapWeight,
-                              const float spaceWeight,
-                              const float abbreviation_scale):
+cpdef vector[float] affinaGapDistanceArray(vector[int[::1]] strings):
     len_list_strings = strings.size()
     size = len_list_strings * (len_list_strings - 1) / 2
     cdef vector[float] out = vector[float](size)
     k = 0
     for i in range(len_list_strings):
         for j in range(i+1, len_list_strings):
-            out[k] = (affineGapDistance(strings[i], strings[j],
-                              gapWeight,
-                              spaceWeight,
-                              abbreviation_scale))
+            out[k] = (affineGapDistance(strings[i], strings[j]))
             k += 1
     return out
